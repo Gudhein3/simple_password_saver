da31070c887e879129b31767db909aa613dee333
Gudhein3 <nikolasshevelkos@gmail.com>
2026-07-26 14:25:24 +0300

    Do thing.

--- simple_password_saver.c
+++ simple_password_saver.c
@@ -10,6 +10,9 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <time.h>
+
+#define CURRENT_PATCH 0
 
 #ifndef AES128
 #error "Bad AES library"
@@ -21,23 +24,39 @@ static_assert(SHA1_DIGEST_BYTE_LENGTH == 20, "Bad SHA1 library");
 // Home directory-relative
 #define PASSWORDS_DIRECTORY "passwords"
 
-typedef struct { // AES Encrypted
-    uint8_t hash[20];   // SHA1 hash of the data.
+#define malloc_check(ptr) do {fall_iff((ptr) != NULL, "Buy more RAM lol");} while(0)
+
+typedef struct {
+    uint8_t patch;      // Lastest patch applied
     uint64_t size;      // Excluding panding
     uint64_t file_size; // Including panding
     uint8_t data[];
 } __attribute__((packed)) Secret;
+
+// Obsolete
+typedef struct {
+    uint8_t hash[20];   // Security risk
+    uint64_t size;      // Excluding panding
+    uint64_t file_size; // Including panding
+    uint8_t data[];
+} __attribute__((packed)) Secret0;
+
+void fall_iff(int cond, const char *message) {
+    if (!cond) {
+        fprintf(stderr, message);
+        abort();
+    }
+}
+
 struct termios term;
 int original_term_c_lflag;
 
-
-void get_lepasswd(char password[MAX_PASSWORD_LENGTH+1], const char *prompt) {
+void get_lepasswd_base(char password[MAX_PASSWORD_LENGTH+1]) {
     term.c_lflag &= ~(ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &term);
-    fprintf(stderr, "%s", prompt);
     {
         int i;
-        for (i = 0;; ++i) { // Consuming all pending characters, so our customer wouldn't accidentally show their password when they entered too many characters. TODO: Explain it better in the code.
+        for (i = 0;; ++i) {
             char ch = getchar();
             if (i < MAX_PASSWORD_LENGTH) {
                 password[i] = ch;
@@ -49,6 +68,43 @@ void get_lepasswd(char password[MAX_PASSWORD_LENGTH+1], const char *prompt) {
 
     term.c_lflag = original_term_c_lflag;
     tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+void get_lepasswd(char password[MAX_PASSWORD_LENGTH+1], const char *prompt) {
+    char password_rep[MAX_PASSWORD_LENGTH+1];
+    fprintf(stderr, "%s", prompt);
+    get_lepasswd_base(password);
+    fprintf(stderr, "Please repeat again\n");
+    get_lepasswd_base(password_rep);
+    if (strcmp(password, password_rep) != 0) {
+        fprintf(stderr, "Sorry, passwords don't match\n");
+        exit(1);
+    }
+    memset(password_rep, 0, sizeof(password_rep));
+}
+
+// I haven't found any libc-native solutions.
+void readline(size_t size;
+              char buffer[size], size_t size) {
+    size_t i;
+    for (i = 0; i < size-1; ++i) {
+        char c;
+        if (fread(&c, 1, 1, stdin) == 0) break;
+        if (c == '\n' || c == '\r') break;
+        buffer[i] = c;
+    }
+    buffer[i] = 0;
+}
+
+int are_you_sure(const char *prompt) {
+    srand(time(NULL)); // Not meant to be cryptographically secure.
+    int x = rand()%10000;
+    fprintf(stderr, "%s Type %d and press enter if so\n", prompt, x);
+    char buf[7/*To make sure it 100% won't overfill*/] = {0};
+    char buf2[7] = {0};
+    snprintf(buf, 6, "%d", x);
+    readline(buf2, sizeof(buf2));
+    return strcmp(buf, buf2) == 0;
 }
 
 void encrypt(const char *key, uint8_t *data, size_t size) {
@@ -74,16 +130,41 @@ void decrypt(const char *key, uint8_t *data, size_t size) {
 }
 
 void usage(const char *program_name) {
-    fprintf(stderr, "Usage: %s ACTION secret's-name\n"
+    fprintf(stderr, "Usage: %s ACTION\n"
                     "\tactions:\n"
-                    "\t  decrypt   - Decrypt the secret\n"
-                    "\t  encrypt   - Encrypt the secret\n"
-                    "\t  recrypt  - Reëncrypt the secret\n"
-                    "\t  remove    - Remove the secret\n"
-                    "\t  list      - List all saved secrets\n", program_name);
+                    "\t  decrypt name   - Decrypt a secret\n"
+                    "\t  encrypt name   - Encrypt a secret\n"
+                    "\t  recrypt name   - Reëncrypt a secret\n"
+                    "\t  fix     name   - Fix a secret\n"
+                    "\t  fix0    name   - Fix a secret using the #0 patch\n"
+                    "\t  remove  name   - Remove a secret\n"
+                    "\t  list           - List all saved secrets\n", program_name);
 }
 
 char current_directory[4096]; // 4096 characters in an absolute path should be enough for everybody.
+
+void verify_secret(Secret *secret) {
+    if (secret->patch > CURRENT_PATCH) {
+        fprintf(stderr, "Aborted: the file has #%d patch, while the manager supports only #%d\n", secret->patch, CURRENT_PATCH);
+        exit(1);
+    }
+
+    int corruption_level = (secret->file_size != secret->size+16-(secret->size&15)<<0);
+
+    if (corruption_level != 0) {
+        fprintf(stderr, "Sorry, your secret file seems to be corrupted with corruption code 0o%o.\n"
+                        "Perhaps you suddenly overwrote the file,\n"
+                        "Or you forgot to apply some patches :)", corruption_level);
+        exit(1);
+    }
+
+    for (size_t i = secret->size; i < secret->file_size; ++i) {
+        if (secret->data[i] != 69) {
+            fprintf(stderr, "Secret cannot be decrypted. It may be caused by file corruption or entering an incorrect password\n");
+            exit(1);
+        }
+    }
+}
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -135,7 +216,9 @@ int main(int argc, char **argv) {
         Decrypt,
         Encrypt,
         Recrypt,
-        Remove
+        Remove,
+        Fix,
+        Fix0
     } action;
     if (strcmp(argv[1], "encrypt") == 0) {
         if (argc != 3) {
@@ -169,6 +252,22 @@ int main(int argc, char **argv) {
         }
         action = Remove;
     }
+    else if (strcmp(argv[1], "fix") == 0) {
+        if (argc != 3) {
+            usage(argv[0]);
+            fprintf(stderr, "Expected secret's name\n");
+            return 2;
+        }
+        action = Fix;
+    }
+    else if (strcmp(argv[1], "fix0") == 0) {
+        if (argc != 3) {
+            usage(argv[0]);
+            fprintf(stderr, "Expected secret's name\n");
+            return 2;
+        }
+        action = Fix0;
+    }
     else if (strcmp(argv[1], "list") == 0) {
         DIR *dir = opendir(".");
         if (!dir) {
@@ -188,7 +287,7 @@ int main(int argc, char **argv) {
         return 2;
     }
 
-    // Modifying argv might look illegal, but it is in our memory, hence everything is legal.
+    // Modifying argv might look illegal, but it is in our memory, so everything is nice :)
     for (char *it = argv[2]; *it; ++it) {
         if (*it == '/') {
             *it = '\\';
@@ -199,11 +298,7 @@ int main(int argc, char **argv) {
     }
 
     if (action == Remove) {
-        fprintf(stderr, "Do You REALLY WANT to REMOVE THE SECRET? Type 49406 if so\n");
-
-        char buf[5] = {0};
-        fread(buf, 1, 6, stdin);
-        if (memcmp(buf, "49406\n", 5) != 0) {
+        if (!are_you_sure("Do you really want to REMOVE the secret?")) {
             fprintf(stderr, "Aborted\n");
             return 1;
         }
@@ -212,6 +307,77 @@ int main(int argc, char **argv) {
             fprintf(stderr, "Failed to unlink(remove) file: %s: %s\n", argv[2], strerror(errno));
             return 2;
         }
+        return 0;
+    }
+    else if (action == Fix0) {
+        if (!are_you_sure("Are you sure this password should be fixed using the #0 patch? You cannot undo the action.")) {
+            fprintf(stderr, "Aborted\n");
+            return 1;
+        }
+        FILE *the_file = fopen(argv[2], "rb");
+        if (!the_file) {
+            fprintf(stderr, "Failed to open file: %s: %s\n", argv[2], strerror(errno));
+            return 2;
+        }
+
+        fseek(the_file, 0, SEEK_END); // Evil offset hack. Don't touch it unless you know what you're about to do.
+        size_t the_size = ftell(the_file);
+        fall_iff(the_size > 19, "wtf?");
+        the_size -= 19;
+        fseek(the_file, 19, SEEK_SET);
+        Secret *secret = malloc(the_size);
+        malloc_check(secret);
+        fread(secret, 1, the_size, the_file);
+        secret->patch = 0;
+
+        fclose(the_file);
+        the_file = fopen(argv[2], "wb"); // Purely for truncation purposes(TBD: I don't know how to spell it properly and I don't have internet to check)
+        if (!the_file) {
+            fprintf(stderr, "Failed to open file: %s: %s\n", argv[2], strerror(errno));
+            return 2;
+        }
+        fwrite(secret, 1, the_size, the_file);
+
+        return 0;
+    }
+    else if (action == Fix) {
+        FILE *the_file = fopen(argv[2], "rb");
+        if (!the_file) {
+            fprintf(stderr, "Failed to open file: %s: %s\n", argv[2], strerror(errno));
+            return 2;
+        }
+
+        fseek(the_file, 0, SEEK_END); // Evil offset hack. Don't touch it unless you know what you're about to do.
+        size_t the_size = ftell(the_file);
+        fall_iff(the_size > 19, "wtf?");
+        the_size -= 19;
+        fseek(the_file, 19, SEEK_SET);
+        Secret *secret = malloc(the_size);
+        malloc_check(secret);
+        fread(secret, 1, the_size, the_file);
+
+        if (secret->patch == CURRENT_PATCH) {
+            fprintf(stderr, "Aborted: already patched on #%d\n", CURRENT_PATCH);
+            return 1;
+        }
+        else if (secret->patch > CURRENT_PATCH) {
+            fprintf(stderr, "Aborted: the file has #%d patch, while the manager supports only #%d\n", secret->patch, CURRENT_PATCH);
+            return 1;
+        }
+
+        if (!are_you_sure("Are you sure this password should be fixed using the #0 patch? You cannot undo the action.")) {
+            fprintf(stderr, "Aborted\n");
+            return 1;
+        }
+
+        fclose(the_file);
+        the_file = fopen(argv[2], "wb"); // Purely for truncation purposes(TBD: I don't know how to spell it properly and I don't have internet to check)
+        if (!the_file) {
+            fprintf(stderr, "Failed to open file: %s: %s\n", argv[2], strerror(errno));
+            return 2;
+        }
+        fwrite(secret, 1, the_size, the_file);
+
         return 0;
     }
     tcgetattr(STDIN_FILENO, &term);
@@ -224,10 +390,7 @@ int main(int argc, char **argv) {
         FILE *output_file = fopen(argv[2], "rb");
         if (output_file) {
             fclose(output_file);
-            fprintf(stderr, "Wait, the secret is already listed, do you want to rewrite it? Type 2026 if so\n");
-            char buf[5] = {0};
-            fread(buf, 1, 5, stdin);
-            if (memcmp(buf, "2026\n", 5) != 0) {
+            if (!are_you_sure("Wait, the secret is already listed, do you want to rewrite it?")) {
                 fprintf(stderr, "Aborted\n");
                 return 1;
             }
@@ -242,6 +405,7 @@ int main(int argc, char **argv) {
         size_t capacity = block_size;
         size_t count = 0;
         uint8_t *data = malloc(capacity);
+        malloc_check(data);
         if (!data) {
             fprintf(stderr, "Failed to alloc memory: %s\n", strerror(errno));
             return 1;
@@ -271,9 +435,10 @@ int main(int argc, char **argv) {
 
         size_t count_aligned = count+16-(count&15);
         Secret *secret = malloc(sizeof(Secret)+count_aligned);
+        malloc_check(secret);
+        secret->patch = 0;
         memcpy(secret->data, data, count);
-        memset(secret->data+count, 69, count_aligned-count); // TODO: Maybe fill padding with random garbage.
-        sha1_digest(secret->data, count, secret->hash);
+        memset(secret->data+count, 69, count_aligned-count); // Password validation code.
         secret->size = count;
         secret->file_size = count_aligned;
 
@@ -282,7 +447,8 @@ int main(int argc, char **argv) {
     }
     else if (action == Decrypt) {
         char password[MAX_PASSWORD_LENGTH+1];
-        get_lepasswd(password, "Please, enter your master password\n");
+        fprintf(stderr, "Please, enter your master password\n");
+        get_lepasswd_base(password);
         FILE *input_file = fopen(argv[2], "rb");
         if (!input_file) {
             fprintf(stderr, "Failed to open file: %s: %s\n", argv[2], strerror(errno));
@@ -293,20 +459,18 @@ int main(int argc, char **argv) {
         size_t input_size = ftell(input_file);
         fseek(input_file, 0, SEEK_SET);
         Secret *secret = malloc(input_size);
+        malloc_check(secret);
         fread(secret, 1, input_size, input_file);
-
         decrypt(password, secret->data, secret->file_size);
+        verify_secret(secret);
+
         uint8_t hash[20];
-        sha1_digest(secret->data, secret->size, hash);
-        if (memcmp(secret->hash, hash, 20) != 0) {
-            fprintf(stderr, "Invalid password\n");
-            return 1;
-        }
         fwrite(secret->data, 1, secret->size, stdout);
     }
     else if (action == Recrypt) {
         char password[MAX_PASSWORD_LENGTH+1];
-        get_lepasswd(password, "Please, enter your old master password\n");
+        fprintf(stderr, "Please, enter your old master password\n");
+        get_lepasswd_base(password);
         FILE *the_file = fopen(argv[2], "r+b");
         if (!the_file) {
             fprintf(stderr, "Failed to open file: %s: %s\n", argv[2], strerror(errno));
@@ -317,16 +481,14 @@ int main(int argc, char **argv) {
         size_t the_size = ftell(the_file);
         fseek(the_file, 0, SEEK_SET);
         Secret *secret = malloc(the_size);
+        malloc_check(secret);
         fread(secret, 1, the_size, the_file);
 
         decrypt(password, secret->data, secret->file_size);
-        uint8_t hash[20];
-        sha1_digest(secret->data, secret->size, hash);
-        if (memcmp(secret->hash, hash, 20) != 0) {
-            fprintf(stderr, "Invalid password\n");
-            return 1;
-        }
-        get_lepasswd(password, "Please, enter your new master password\n");
+        verify_secret(secret);
+
+        fprintf(stderr, "Please, enter your new master password\n");
+        get_lepasswd_base(password);
         encrypt(password, secret->data, secret->file_size);
         fseek(the_file, 0, SEEK_SET);
         fwrite(secret, 1, the_size, the_file);
